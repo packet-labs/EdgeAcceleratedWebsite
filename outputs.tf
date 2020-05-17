9cf38830e31acdaa238402e26555a993f571a578
John Studarus <john@jhlconsulting.com>
2020-05-17 16:33:50 -0700

    ipv6 output for validation that things are running correctly

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@
 output "deployed_edge_proxies_ipv4" {
   value = module.EdgeAccelerator.deployed_edge_proxies_ipv4
 }
+
+output "deployed_edge_proxies_ipv6" {
+  value = module.EdgeAccelerator.deployed_edge_proxies_ipv6
+}
