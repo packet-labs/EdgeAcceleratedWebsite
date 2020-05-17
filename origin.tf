5565552444eafb1986d4704d353a4d5d68a6cd7f
John Studarus <john@jhlconsulting.com>
2020-05-17 16:34:23 -0700

    more interesting static content on the origin server

--- origin.tf
+++ origin.tf
@@ -12,6 +12,43 @@ resource "packet_ssh_key" "packet_key" {
   public_key = tls_private_key.key.public_key_openssh
 }
 
+# listen only on IPv6
+data "template_file" "static-ipv6-website-conf" {
+
+  template =<<EOT
+server {
+    listen [::]:80;
+
+    root /var/www/static-ipv6-website/;
+    index index.html;
+
+    location / {
+      try_files $uri $uri/ =404;
+    }
+}
+EOT
+}
+
+data "template_file" "static-ipv6-website" {
+
+  template =<<EOT
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Hello, Edge Accelerator User!</title>
+</head>
+<body>
+    <h1>Hello, Edge Accelerator User!</h1>
+    <p>This is a sample website to illustrate pulling content from edge accelerators/reverse proxies.<p>
+</body>
+</html>
+EOT
+
+  vars = {
+  }
+}
+
 resource "packet_device" "origin" {
   depends_on       = [packet_ssh_key.packet_key]
 
@@ -46,7 +83,24 @@ resource "packet_device" "origin" {
     inline = [
       "apt-get -y update", 
       "apt-get -y install nginx",
+      "unlink /etc/nginx/sites-enabled/default",
+      "mkdir -p /var/www/static-ipv6-website/",
+    ]
+  }
+
+  provisioner "file" {
+    content     = data.template_file.static-ipv6-website-conf.rendered
+    destination = "/etc/nginx/sites-enabled/static-ipv6-website.conf"
+  }
+
+  provisioner "file" {
+    content     = data.template_file.static-ipv6-website.rendered
+    destination = "/var/www/static-ipv6-website/index.html"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "service nginx restart",
     ]
   }
 }
-
